5d843a5d5dd7daff2b24743fc7d2b80913e3ff12
Titi Wangsa Damhore <ttwd80@gmail.com>
2016-02-20 11:59:34 +1100

    proper date-stamp value for today

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function() {
 	$('#link-timestamp-today-utc').click(function() {
 		var now = moment.utc();
-		var value = now.format("dddd, MMMM Do YYYY, h:mm:ss a");
+		var value = now.format("YYYYMMDD");
 		$('#date-stamp').val(value);
 	});
 });
