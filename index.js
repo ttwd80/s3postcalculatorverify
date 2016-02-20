b9abc0b605e7a0e3feeb302aa9b9c47488889472
Titi Wangsa Damhore <ttwd80@gmail.com>
2016-02-20 12:17:36 +1100

    required validation

--- index.js
+++ index.js
@@ -4,4 +4,34 @@ $(function() {
 		var value = now.format("YYYYMMDD");
 		$('#date-stamp').val(value);
 	});
+
+	$('#button-calculate').click(function() {
+		var errors = validate();
+		if (errors.length > 0) {
+			display_error_messages(errors);
+		} else {
+
+		}
+	});
+
+	function validate() {
+		var list = [];
+		if ($('#policy').val().length == 0) {
+			list.push('Policy is empty.')
+		}
+		if ($('#secret-access-key').val().length == 0) {
+			list.push('Secret access key is empty.')
+		}
+		if ($('#region').val().length == 0) {
+			list.push('Region is empty.')
+		}
+		if ($('#service').val().length == 0) {
+			list.push('Region is empty.')
+		}
+	}
+
+	function display_error_messages(list) {
+		var message = list.join("\n");
+		alert(message);
+	}
 });
