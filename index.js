4f344b6adb92779d243aeae2d3560cf36c9ee955
Titi Wangsa Damhore <ttwd80@gmail.com>
2016-02-20 12:44:08 +1100

    step 2 ok

--- index.js
+++ index.js
@@ -10,9 +10,32 @@ $(function() {
 		if (errors.length > 0) {
 			display_error_messages(errors);
 		} else {
-
+			show_steps();
 		}
 	});
+
+	function show_steps() {
+		$("#steps").empty();
+		step_1();
+		step_2();
+	}
+
+	function step_1() {
+		var label = $("<div>Step 1</div>");
+		var content = $("<div></div>").append($('#policy').val());
+		$("#steps").append(label);
+		$("#steps").append(content);
+	}
+
+	function step_2() {
+		var label = $("<div>Step 2</div>");
+		var plaintext = "AWS4" + $("#secret-access-key").val();
+		var array = CryptoJS.enc.Utf8.parse(plaintext);
+		var text = CryptoJS.enc.Hex.stringify(array);
+		var content = $("<div></div>").append(text);
+		$("#steps").append(label);
+		$("#steps").append(content);
+	}
 
 	function validate() {
 		var list = [];
@@ -21,6 +44,9 @@ $(function() {
 		}
 		if ($('#secret-access-key').val().length == 0) {
 			list.push('Secret access key is empty.')
+		}
+		if ($('#date-stamp').val().length == 0) {
+			list.push('Date stamp is empty.')
 		}
 		if ($('#region').val().length == 0) {
 			list.push('Region is empty.')
