2d57c98f90cdcbcec15a6490e71190f489372e33
Titi Wangsa Damhore <ttwd80@gmail.com>
2016-02-20 11:58:19 +1100

    test moment utc

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(function() {
 	$('#link-timestamp-today-utc').click(function() {
-		alert('hit!');
+		var now = moment.utc();
+		var value = now.format("dddd, MMMM Do YYYY, h:mm:ss a");
+		$('#date-stamp').val(value);
 	});
 });
